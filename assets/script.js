fd2d152ca629d6235f51318dc337dc8e385170b4
IVIonsters <zacharypolof@gmail.com>
2024-03-31 10:47:55 -0500

    added basic prompts for 2 integers, added functions to add , subtract, multiply , divide, and log to console - Revist in future for full calculator demonstration

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,47 @@
-const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-const randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-function listTopics() {
-  for (let x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
-  }
-}
+// Define variables for prompts
+let first = prompt('enter first number?');
+let second = prompt('enter second number?');
+console.log("The first number is:" + " " + first, "The second numbers is" + " " + second);
 
-function selectTopic() {
-  if (randomTopic === 'HTML') {
-  console.log("Let's study HTML!");
-  } else if (randomTopic === 'CSS') {
-  console.log("Let's study CSS!");
-  } else if (randomTopic === 'Git') {
-  console.log("Let's study Git!");
-  } else if (randomTopic === 'JavaScript') {
-  console.log("Let's study JavaScript!");
-  } else {
-  console.log('Please try again!');
-  }
-}
+// Convert strings to numbers and calculate sum: log answer
+let sum = Number(first) + Number(second);
+alert("The sum is:" + " " + sum);
+console.log("The sum is:" + " " + sum);
 
-console.log('Here are the topics we leanred through Prework:');
-listTopics();
-console.log('Which topic should we study first?');
-selectTopic();
+// calculate difference: log answer
+let difference = Number(first) - Number(second);
+alert("The difference is:" + " " + difference);
+console.log("The difference is:" + " " + difference);
+
+// calculate product: log answer
+let product = Number(first) * Number(second);
+alert("The product is:" + " " + product);
+console.log("The product is:" + " " + product);
+
+// calculate quotient: log answer
+let quotient = Number(first) / Number(second);
+alert("The quotient is:" + " " + quotient);
+console.log("The quotient is:" + " " + quotient);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
