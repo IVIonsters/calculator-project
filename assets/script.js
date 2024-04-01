73d3c61513c6ef43933a668ebdb657296fee2b27
IVIonsters <zacharypolof@gmail.com>
2024-03-31 22:38:36 -0500

    added class and id tags to body section, added buttons with append fucntions, calculate function and clear function

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,4 @@
 
-// Define variables for prompts
-let first = prompt('enter first number?');
-let second = prompt('enter second number?');
-console.log("The first number is:" + " " + first, "The second numbers is" + " " + second);
-
-// Convert strings to numbers and calculate sum: log answer
-let sum = Number(first) + Number(second);
-alert("The sum is:" + " " + sum);
-console.log("The sum is:" + " " + sum);
-
-// calculate difference: log answer
-let difference = Number(first) - Number(second);
-alert("The difference is:" + " " + difference);
-console.log("The difference is:" + " " + difference);
-
-// calculate product: log answer
-let product = Number(first) * Number(second);
-alert("The product is:" + " " + product);
-console.log("The product is:" + " " + product);
-
-// calculate quotient: log answer
-let quotient = Number(first) / Number(second);
-alert("The quotient is:" + " " + quotient);
-console.log("The quotient is:" + " " + quotient);
-
 
 
 
