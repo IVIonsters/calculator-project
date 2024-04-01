425f53abd5389471a434b4228875d334e7df3bc4
IVIonsters <zacharypolof@gmail.com>
2024-03-31 23:02:54 -0500

    defined functions for append, clear, and calculate. added try / catch statement to control errors

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,30 @@
+// Calc Program
+
+const display = document.getElementById("display");
+
+// Function for appending value to display
+function appendtoDisplay(input) {
+    display.value += input;
+}
 
 
 
+// Function for clearing the display
+function clearDisplay() {
+    display.value = "";
+}
 
 
 
+// Function for calculating the result
+function calculate() {
+    try {
+    display.value = eval(display.value);
+    }
+    catch {
+        display.value = "Error";
+    }
+}
 
 
 
